257c795dc86f72eae181f8ed2ef1e28e8362e684
quinoa <34537088+xenobinary@users.noreply.github.com>
2026-03-17 16:42:58 -0400

    Update test_runner.pl to include clpfd_solver tests

--- tests/test_runner.pl
+++ tests/test_runner.pl
@@ -3,11 +3,9 @@
 :- ensure_loaded('test_parser.pl').
 :- ensure_loaded('test_constructive.pl').
 :- ensure_loaded('test_hard_constraints_clpfd.pl').
-
-run_tests :-
-    run_tests([parser, constructive, hard_constraints_clpfd]),
 :- ensure_loaded('test_clpfd_solver.pl').
 
 run_tests :-
+    run_tests([parser, constructive, hard_constraints_clpfd]),
     run_tests([parser, constructive, clpfd_solver]),
     halt(0).
